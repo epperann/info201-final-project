93a72941bb423e3cae8ed2baa9bca1940d5b3a1b
John Duffy <johnduffy@MacBook-Pro-4.local>
2021-05-31 14:37:33 -0700

    created a shiny app with a description of data and map of the world

---
+++ happiness-app/server.R
@@ -0,0 +1,36 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+
+data <- read.csv("data/world-happiness-report-2021.csv")
+
+# Define server logic required to draw a map
+shinyServer(function(input, output) {
+
+    
+    output$message <- renderText({ 
+        description <- "The data we are analyzing is the world happiness report from 2021. 
+            The data set uses factors such as GDP, social support, life expectancy, freedom, and corruption to 
+            determine the happiness of a country’s citizens. Also, they use their own ladder score. 
+            This variable is meant to be representative of how easy it is for one to improve one’s own life. 
+            A country with a high ladder score makes it easier for citizens to advance themselves, 
+            and lower scoring countries do the opposite."
+        })
+    
+    output$dataPlot <- renderPlot({
+        ggplot(map_data("world"), aes(long, lat, group = group)) + 
+            geom_polygon(col = "black", size = .1, fill = "white") +
+            coord_quickmap()
+    
+
+    })
+
+})
